a13238eec561616516266999338e9d2b83132ad7
Masaki Komagata <komagata@gmail.com>
2017-03-31 18:03:10 +0900

    Support namespace

--- lib/view_id/helper.rb
+++ lib/view_id/helper.rb
@@ -1,11 +1,11 @@
 module ViewId
   module Helper
     def view_id
-      view_identifier(controller_name, action_name)
+      view_identifier(controller.class.to_s, action_name)
     end
 
     def view_identifier(controller_name, action_name)
-      "#{controller_name.gsub(/::/, "-")}--#{action_name}"
+      "#{controller_name.gsub(/Controller/, "").underscore.gsub(%r{/}, "-")}--#{action_name}"
     end
   end
 end
